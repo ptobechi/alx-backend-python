aaa5ccddfd010a85064dbc941da193999dc359e7
ptobechi <ptobechii@gmail.com>
2024-06-06 09:34:11 -0500

    add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,26 +1,15 @@
 #!/usr/bin/env python3
-
 """
-This module defines the Student class.
+This module provides a function to add two float numbers.
 """
 
 
-class Student:
+def add(a: float, b: float) -> float:
     """
-    This class represents a student.
+    Adds two float numbers and returns their sum.
+
+    :param a: The first float number.
+    :param b: The second float number.
+    :return: The sum of the two float numbers.
     """
-
-    def __init__(self, first_name: str, last_name: str, age: int, location: str):
-        """
-        Initialize a new Student object.
-
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-            location (str): The location of the student.
-        """
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-        self.location = location
+    return a + b
