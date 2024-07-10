51735f54e560fed6346f19ae92eebbc8f593d8b3
ptobechi <ptobechii@gmail.com>
2024-07-10 15:14:16 -0500

    task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,9 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+this module is a Parameterize a unit test
+"""
+
 import unittest
-__import__('parameterized').parameterized
-__import__('utils').access_nested_map
+from parameterized import parameterized
+from utils import access_nested_map  # Import your function here
 
 
 class TestAccessNestedMap(unittest.TestCase):
+    """
+    Test case for the access_nested_map function.
+    """
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -11,7 +20,18 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2),
         ])
     def test_access_nested_map(self, nested_map, path, expected):
-        self.assertEqual(access_nested_map(nested_map, path), expected)
+        """
+        Test access_nested_map function with various inputs.
 
-if __name__ == "__main__":
+        Args:
+            nested_map (dict): The nested dictionary to access.
+            path (tuple): The path of keys to access within the
+            nested dictionary.expected:
+            The expected result of accessing the nested dictionary
+            with the given path.
+        """
+        result = access_nested_map(nested_map, path)
+        self.assertEqual(result, expected)
+
+if __name__ == '__main__':
     unittest.main()
